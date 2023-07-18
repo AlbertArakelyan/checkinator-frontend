90983c6b123302114ff40066ab962467af2de317
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-07-18 06:19:22 +0400

    test commit for test push

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem veniam commodi accusantium consequent
-        quibusdam! Inventore beatae dolor iste recusandae animi at, voluptatibus alias aperiam est ut amet molestiae
-        consequuntur officiis quam, aliquam, eos accusamus sunt quis! Nesciunt, hic. Unde quisquam eos ut, laboriosam,
-        magnam dolorem a iusto ea aliquid reiciendis illo!
+        Hello world
       </div>
     </div>
   );
