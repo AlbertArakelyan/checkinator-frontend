bd179003e901e8f0a6b32b29d63229ab73f10dcd
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-07-26 19:03:22 +0300

    fix(1): fixed failed tests

--- src/components/UI/Avatar/AvatarContainer.tsx
+++ src/components/UI/Avatar/AvatarContainer.tsx
@@ -5,7 +5,7 @@ import Avatar from './Avatar';
 import { IAvatarContainerProps } from './types';
 
 const AvatarContainer: FC<IAvatarContainerProps> = ({ src }) => {
-  return <Avatar />;
+  return <Avatar src={src} />;
 };
 
 export default AvatarContainer;
