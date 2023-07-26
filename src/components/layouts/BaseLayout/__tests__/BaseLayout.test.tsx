a7442a73bb35a15f9a1016ad8617320e77811625
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-07-26 19:43:44 +0300

    feat(8): added tests for BaseLayout

---
+++ src/components/layouts/BaseLayout/__tests__/BaseLayout.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+
+import BaseLayout from '../BaseLayout';
+
+let container: HTMLElement;
+
+
+describe('BaseLayout', () => {
+  beforeEach(() => {
+    container = render(<BaseLayout />).container;
+  })
+
+  it('should have 1 main tag inside', () => {
+    expect(container.querySelectorAll('main').length).toBe(1);
+  });
+
+  it('should have 1 header tag inside', () => {
+    expect(container.querySelectorAll('header').length).toBe(1);
+  });
+});
