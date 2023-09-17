74fc2665e0fc185f7ac84f377d7d799d722a47e8
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-09-17 17:09:36 +0400

    fix(41): linter fixes

--- src/pages/user/Home/__tests__/Home.test.tsx
+++ src/pages/user/Home/__tests__/Home.test.tsx
@@ -6,7 +6,7 @@ import SubscriptionCard from '../components/SubscriptionCard/SubscriptionCard';
 import { mockPlansData } from 'tests/constants';
 
 import { VariantType } from 'types';
-import {SubscriptionCardFeature} from "../components/SubscriptionCard/components";
+import { SubscriptionCardFeature } from '../components/SubscriptionCard/components';
 
 let container: HTMLElement;
 let component: any;
